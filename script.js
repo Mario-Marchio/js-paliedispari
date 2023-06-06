14b2b9b197810e6858d0a049a7a7d7e493c6827e
Mario-Marchio <mariomarchio1610@gmail.com>
2023-06-06 20:30:14 +0200

    palindrome word done

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 
+function Palindroma(word) {
+    const betterWord = word.toLowerCase().trim();
+    
+    for (let i = 0; i < betterWord.length / 2; i++) {
+      if (betterWord[i] === betterWord[length - 1 - i]) {
+        return true;
+      }else{
+        return false;
+      }
+    }
+    
+  }
 
-
-
-const name = prompt(`inserisci il tuo nome`,`beppe`);
-console.log(name);
+  const name = prompt(`Inserisci il tuo nome:`);
+  
+  const reverseWord = Palindroma(name);
+  
+  if (reverseWord) {
+    console.log(`La parola inserita è palindroma.`);
+  } else {
+    console.log(`La parola inserita non è palindroma.`);
+  }
