b4aa1dd953c6e98abb7cd03cccd66a1d4d5b2542
Mario-Marchio <mariomarchio1610@gmail.com>
2023-06-06 20:31:38 +0200

    adjust the if

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function Palindroma(word) {
     const betterWord = word.toLowerCase().trim();
     
     for (let i = 0; i < betterWord.length / 2; i++) {
-      if (betterWord[i] === betterWord[length - 1 - i]) {
+      if (betterWord[i] === betterWord[betterWord.length - 1 - i]) {
         return true;
       }else{
         return false;
